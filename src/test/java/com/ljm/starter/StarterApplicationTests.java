44a614907460eb58fb4e04b056779c570a9b2898
lijianming <489442270@qq.com>
2023-11-12 14:44:56 +0800

    Initial commit

---
+++ src/test/java/com/ljm/starter/StarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljm.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
